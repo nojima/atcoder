4f661ff28f1a6e933dbb9c7915bfd25e39135c69
Yusuke Nojima <nojima@ynojima.com>
2020-04-29 21:58:23 +0900

    ABC163 D

--- ABC163/D.cpp
+++ ABC163/D.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
-#include <algorithm>
-#include <cstdio>
-#include <cstring>
-#include <string>
-#include <vector>
-#include <cassert>
+#include <cstdint>
 using namespace std;
 
 int main() {
+    int64_t N, K; cin >> N >> K;
+
+    int64_t ans = 0;
+    for (int64_t k = K; k <= N+1; ++k) {
+        ans += (N-k+1)*k+1;
+        ans %= 1000000007;
+    }
+    cout << ans << "\n";
 }
