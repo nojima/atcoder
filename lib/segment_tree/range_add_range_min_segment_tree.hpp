dd45ba4f0117c528cb58631c3d52c22cdc56d69f
Yusuke Nojima <nojima@ynojima.com>
2020-09-06 13:03:32 +0900

    Do not use optional

--- lib/segment_tree/range_add_range_min_segment_tree.hpp
+++ lib/segment_tree/range_add_range_min_segment_tree.hpp
@@ -1,7 +1,6 @@
 #include <vector>
 #include <algorithm>
 #include <limits>
-#include <optional>
 
 int bit_ceil(int n) {
     int ret = 1;
@@ -15,10 +14,10 @@ template <typename Num>
 struct RangeAddRangeMinSegmentTree {
     int n;
     vector<Num> data;
-    vector<optional<Num>> delayed;
+    vector<Num> delayed;
 
     explicit RangeAddRangeMinSegmentTree(int size)
-        : n(bit_ceil(size)), data(2*n-1), delayed(2*n-1, nullopt) {}
+        : n(bit_ceil(size)), data(2*n-1), delayed(2*n-1) {}
 
     // [lo, hi) の区間の値に value を加算する
     void range_add(int lo, int hi, Num value) {
@@ -64,20 +63,13 @@ private:
     }
 
     void force(int index) {
-        if (!delayed[index]) return;
+        if (delayed[index] == 0) return;
         if (index < n-1) {
-            add_delayed_value(index*2+1, *delayed[index]);
-            add_delayed_value(index*2+2, *delayed[index]);
+            data[index*2+1] += delayed[index];
+            data[index*2+2] += delayed[index];
+            delayed[index*2+1] += delayed[index];
+            delayed[index*2+2] += delayed[index];
         }
-        delayed[index] = nullopt;
-    }
-
-    void add_delayed_value(int index, int value) {
-        data[index] += value;
-        if (delayed[index]) {
-            *delayed[index] += value;
-        } else {
-            delayed[index] = value;
-        }
+        delayed[index] = 0;
     }
 };
