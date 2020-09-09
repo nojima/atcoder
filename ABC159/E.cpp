487a80d18f81cf7501116cd7c8b2fdcac0d69aab
Yusuke Nojima <nojima@ynojima.com>
2020-09-09 21:37:50 +0900

    update

--- ABC159/E.cpp
+++ ABC159/E.cpp
@@ -1,72 +1,65 @@
-#include <cstdio>
-#include <iostream>
 #include <algorithm>
+#include <iostream>
 #include <vector>
-#include <string>
-#include <climits>
 using namespace std;
 
-inline bool test_bit(unsigned x, int i) {
-    return (x >> i) & 1;
+#define REP(i, n) for (int i = 0; i < (int)(n); ++i)
+#define ALL(c) (c).begin(), (c).end()
+
+inline bool test_bit(unsigned S, int i) {
+    return (i >= 0) ? ((S >> i) & 1) : false;
+}
+
+int best_splits(
+    const vector<vector<bool>>& choco,
+    int H, int W, int K,
+    unsigned S
+) {
+    vector<int> group(H);
+    int current_group = 0;
+    REP(y, H) {
+        if (test_bit(S, y-1)) { current_group++; }
+        group[y] = current_group;
+    }
+    int group_count = current_group + 1;
+    vector<int> n_whites(group_count);
+
+    int v_splits = 0;
+    REP(x, W) {
+        REP(y, H) if (choco[y][x]) {
+            n_whites[group[y]]++;
+        }
+        bool should_split = any_of(ALL(n_whites), [K](int n) { return n > K; });
+        if (should_split) {
+            v_splits++;
+            fill(ALL(n_whites), 0);
+            REP(y, H) if (choco[y][x]) {
+                n_whites[group[y]]++;
+            }
+            bool impossible = any_of(ALL(n_whites), [K](int n) { return n > K; });
+            if (impossible) {
+                return numeric_limits<int>::max();
+            }
+        }
+    }
+
+    return __builtin_popcount(S) + v_splits;
 }
 
 int main() {
+    cin.tie(nullptr); ios::sync_with_stdio(false);
+
     int H, W, K; cin >> H >> W >> K;
-    vector<vector<bool>> choco(H+1, vector<bool>(W+1));
-    for (int y = 1; y <= H; ++y) {
-        for (int x = 1; x <= W; ++x) {
-            char ch; cin >> ch;
-            choco[y][x] = ch == '1';
-        }
+    vector<vector<bool>> choco(H, vector<bool>(W));
+    REP(y, H) REP(x, W) {
+        char s; cin >> s;
+        choco[y][x] = s == '1';
     }
 
-    vector<vector<int>> sum(H+1, vector<int>(W+1));
-    for (int y = 1; y <= H; ++y) {
-        for (int x = 1; x <= W; ++x) {
-            sum[y][x] = sum[y-1][x] + sum[y][x-1] - sum[y-1][x-1];
-        }
+    int ans = numeric_limits<int>::max();
+    for (unsigned S = 0; S < (unsigned)(1 << (H-1)); ++S) {
+        int n_splits = best_splits(choco, H, W, K, S);
+        ans = min(ans, n_splits);
     }
-
-    int ans = INT_MAX;
-    for (unsigned h_slice = 0; h_slice < (unsigned)(1 << H); ++h_slice) {
-        int h_split = __builtin_popcount(h_slice);
-        int v_split = 0;
-        vector<int> count(H+1);
-        vector<int> next(H+1);
-        for (int x = 1; x <= W; ++x) {
-            int cut_y = 1;
-            fill(next.begin(), next.end(), 0);
-            for (int y = 1; y <= H; ++y) {
-                if (test_bit(h_slice, y)) { cut_y = y; }
-                if (choco[y][x]) { next[cut_y]++; }
-            }
-            //cout << "next[1]" << ": " << next[1] << endl;
-            //cout << "count[1]" << ": " << count[1] << endl;
-            for (int y = 1; y <= H; ++y) {
-                if (next[y] > K) {
-                    goto next_candidate;
-                }
-            }
-            bool should_split = false;
-            for (int y = 1; y <= H; ++y) {
-                if (count[y] + next[y] > K) {
-                    should_split = true;
-                }
-                count[y] += next[y];
-            }
-            if (should_split) {
-                v_split++;
-                count = next;
-            }
-        }
-
-        //cout << h_slice << ": " << v_split << endl;
-        if (h_split + v_split < ans) {
-            ans = h_split + v_split;
-        }
-
-next_candidate:;
-    }
-
-    cout << ans << endl;
+    cout << ans << '\n';
 }
