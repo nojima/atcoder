810077fd0464e18ef160e972ac279333047874e0
Yusuke Nojima <nojima@ynojima.com>
2020-09-11 12:12:43 +0900

    update

--- ABC163/E.cpp
+++ ABC163/E.cpp
@@ -7,14 +7,20 @@ using namespace std;
 #define REP(i, n) for (int i = 0; i < (int)(n); ++i)
 #define ALL(c) (c).begin(), (c).end()
 
+template <class T, class Compare>
+vector<int> sort_index(const vector<T>& v, Compare comp) {
+    vector<int> index(v.size());
+    REP(i, v.size()) index[i] = i;
+    sort(ALL(index), [&](int i, int j) { return comp(v[i], v[j]); });
+    return index;
+}
+
 int main() {
     int N; cin >> N;
     vector<int64_t> A(N);
     REP(i, N) cin >> A[i];
 
-    vector<pair<int64_t, int>> indexed(N);
-    REP(i, N) indexed[i] = {A[i], i};
-    sort(ALL(indexed), greater());
+    auto index = sort_index(A, greater());
 
     // dp[L][R] = 左にL人、右にR人配置したときの嬉しさの最大値
     vector dp(N+1, vector(N+1, int64_t()));
@@ -22,7 +28,7 @@ int main() {
         if (L == 0 && R == 0) {
             dp[L][R] = 0;
         } else {
-            auto [_, k] = indexed[L+R-1];
+            auto k = index[L+R-1];
             int64_t vl = L >= 1 ? dp[L-1][R] + A[k] * abs(k+1 - L)
                                 : numeric_limits<int64_t>::min();
             int64_t vr = R >= 1 ? dp[L][R-1] + A[k] * abs(k+1 - (N-R+1))
