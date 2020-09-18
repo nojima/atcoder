cfdf39aed885a25212696759ae01718d2d09b042
Yusuke Nojima <nojima@ynojima.com>
2020-09-18 23:41:17 +0900

    update

--- ABC174/E.cpp
+++ ABC174/E.cpp
@@ -7,15 +7,17 @@ using namespace std;
 #define REP(i, n) for (int i = 0; i < (int)(n); ++i)
 #define ALL(c) (c).begin(), (c).end()
 
-// 区間[a, b)の中で最初に pred(x) = true となるような x を返す
+// 区間[a, b)の中で最初に pred(x) = true となるような x を返す。
+// そのような x が存在しないときは b を返す。
 template <class Pred>
 int64_t bin_search(int64_t a, int64_t b, Pred pred) {
-    if (a + 1 == b) return b;
+    // 不変条件: i<a ⇒ pred(i)=false, i>=b ⇒ pred(i)=true
+    if (a == b) return b;
     int64_t m = a + (b - a) / 2;
     if (pred(m))
         return bin_search(a, m, pred);
     else
-        return bin_search(m, b, pred);
+        return bin_search(m+1, b, pred);
 }
 
 inline int64_t div_ceil(int64_t a, int64_t b) noexcept {
@@ -31,7 +33,7 @@ int main() {
     int64_t S = 0;
     REP(i, N) S += A[i];
 
-    int64_t ans = bin_search(0, S+1, [&](int64_t L) {
+    int64_t ans = bin_search(1, S+1, [&](int64_t L) {
         // 最大の丸太の長さが L 以下になるように切ることができるか
         int64_t k = 0;
         REP(i, N) k += div_ceil(A[i], L) - 1;
