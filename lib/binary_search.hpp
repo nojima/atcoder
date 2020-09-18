0a54b09999ad74511ed806b70c5d5bdc7c3c1024
Yusuke Nojima <nojima@ynojima.com>
2020-09-18 23:48:45 +0900

    update

---
+++ lib/binary_search.hpp
@@ -0,0 +1,12 @@
+// 区間[a, b)の中で最初に pred(x) = true となるような x を返す。
+// そのような x が存在しないときは b を返す。
+template <class Pred>
+int64_t bin_search(int64_t a, int64_t b, Pred pred) {
+    // 不変条件: i<a ⇒ pred(i)=false, i>=b ⇒ pred(i)=true
+    if (a == b) return b;
+    int64_t m = a + (b - a) / 2;
+    if (pred(m))
+        return bin_search(a, m, pred);
+    else
+        return bin_search(m+1, b, pred);
+}
