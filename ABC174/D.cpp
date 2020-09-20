1642b731cd0e94abcf9be264de1158782c5165f1
Yusuke Nojima <nojima@ynojima.com>
2020-09-20 22:56:26 +0900

    update

---
+++ ABC174/D.cpp
@@ -0,0 +1,28 @@
+#include "../lib/prelude.hpp"
+
+int main() {
+    int N = read_int();
+    vector<char> C(N);
+    REP(i, N) C[i] = read_char();
+
+    int total_w = 0, total_r = 0;
+    REP(i, N) {
+        if (C[i] == 'W')
+            ++total_w;
+        else
+            ++total_r;
+    }
+
+    // 0~iをすべてRにし、i+1~N-1をWにしたい
+    // max(前半のWの数, 後半のRの数)
+    auto ans = min(total_w, total_r);
+    int n_w = 0, n_r = 0;
+    REP(i, N) {
+        if (C[i] == 'W')
+            ++n_w;
+        else
+            ++n_r;
+        assign_min(ans, max(n_w, total_r - n_r));
+    }
+    cout << ans << LF;
+}
--- lib/prelude.hpp
+++ lib/prelude.hpp
@@ -8,3 +8,5 @@
 
 template <typename T> inline void assign_min(T& x, const T& value) noexcept { x = min(x, value); }
 template <typename T> inline void assign_max(T& x, const T& value) noexcept { x = max(x, value); }
+
+constexpr char LF = '\n';
