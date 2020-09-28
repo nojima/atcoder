de991e4b3e4b281c08ab5a40cef9fc96d0b0e6d0
Yusuke Nojima <nojima@ynojima.com>
2020-09-28 23:39:42 +0900

    update

--- lib/combination.hpp
+++ lib/combination.hpp
@@ -4,6 +4,7 @@
 template<class Int>
 struct Combination {
     vector<Int> facts = {1};
+    vector<Int> inv_facts;
     Int fact(int n) {
         if (n < (int)facts.size()) { return facts[n]; }
         for (int i = facts.size(); i <= n; ++i) {
@@ -11,6 +12,14 @@ struct Combination {
         }
         return facts[n];
     }
-    Int nCr(int n, int r) { return fact(n) / (fact(r) * fact(n-r)); }
+    Int inv_fact(int n) {
+        if (n < (int)inv_facts.size()) { return inv_facts[n]; }
+        fact(n); // fill facts[0..n]
+        for (int i = inv_facts.size(); i <= n; ++i) {
+            inv_facts.emplace_back(facts[i].inverse());
+        }
+        return inv_facts[n];
+    }
+    Int nCr(int n, int r) { return fact(n) * inv_fact(r) * inv_fact(n-r); }
     Int nHr(int n, int r) { return nCr(n+r-1, r); }
 };
