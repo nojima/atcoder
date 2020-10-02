4a8576b1ad60500aa4d6b89d192492e29a07b0fe
Yusuke Nojima <nojima@ynojima.com>
2020-10-03 02:39:05 +0900

    update

--- lib/segment_tree.hpp
+++ lib/segment_tree.hpp
@@ -1,4 +1,4 @@
-#include "lib/prelude.hpp"
+#pragma once
 #include "lib/bit.hpp"
 
 template<class T, class Product>
@@ -37,31 +37,3 @@ private:
     T identity;
     Product product;
 };
-
-int main() {
-    auto N = read_int();
-    auto K = read_int();
-    vector<int64_t> A(N);
-    REP(i, N) { A[i] = read_int(); }
-
-    int64_t Amax = numeric_limits<int64_t>::min();
-    REP(i, N) { assign_max(Amax, A[i]); }
-
-    // dp[i]: i番目までの条件を満たす最大の長さ
-    vector dp(N, int64_t());
-    SegmentTree st(Amax+1,
-        int64_t(0),
-        [](auto x, auto y){ return max(x, y); });
-    REP(i, N) {
-        dp[i] = 1;
-        if (i > 0) { assign_max(dp[i], dp[i-1]); }
-        // A[j] が A[i]-K .. A[i]+K の範囲に入っているような j に対する dp[j] の最大値
-        int64_t v = st.prod(max<int64_t>(0, A[i]-K), min(Amax+1, A[i]+K+1)) + 1;
-        assign_max(dp[i], v);
-
-        if (st.get(A[i]) < v) {
-            st.set(A[i], v);
-        }
-    }
-    cout << dp[N-1] << LF;
-}
