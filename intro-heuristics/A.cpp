7c51c249ba051d778a6bf82acd3f3a975ac55a0f
Yusuke Nojima <nojima@ynojima.com>
2021-03-08 01:00:01 +0900

    update

--- intro-heuristics/A.cpp
+++ intro-heuristics/A.cpp
@@ -53,8 +53,61 @@ inline double lerp(double x, double y, double alpha) {
     return x + (y - x) * alpha;
 }
 
+inline int64_t calculate_penalty(int k) {
+    return k * (k+1) / 2;
+}
+
+int64_t calculate_score_delta_on_contest_remove(
+    vector<set<int>>& contest_date_list, int day, Contest contest
+) {
+    auto& l = contest_date_list[contest];
+    //cerr << "remove: l = " << l << ", contest = " << contest << endl;
+    auto it = l.find(day);
+    int prev_day = *prev(it);
+    int next_day = *next(it);
+
+    int d1 = next_day - prev_day - 1; // コンテストが開催されなかった日数
+    int d2 = day - prev_day - 1;
+    int d3 = next_day - day - 1;
+
+    int64_t p1 = calculate_penalty(d1);
+    int64_t p2 = calculate_penalty(d2);
+    int64_t p3 = calculate_penalty(d3);
+
+    l.erase(day);
+
+    auto delta = p2 + p3 - p1;
+    //cerr << "remove: delta = " << delta << endl;
+    return delta * C[contest] - S[day][contest];
+}
+
+int64_t calculate_score_delta_on_contest_add(
+    vector<set<int>>& contest_date_list, int day, Contest contest
+) {
+    auto& l = contest_date_list[contest];
+    //cerr << "add: l = " << l << ", contest = " << contest << endl;
+    auto it = l.lower_bound(day);
+    int next_day = *it;
+    int prev_day = *prev(it);
+
+    int d1 = next_day - prev_day - 1; // コンテストが開催されなかった日数
+    int d2 = day - prev_day - 1;
+    int d3 = next_day - day - 1;
+
+    int64_t p1 = calculate_penalty(d1);
+    int64_t p2 = calculate_penalty(d2);
+    int64_t p3 = calculate_penalty(d3);
+
+    l.insert(day);
+
+    auto delta = p1 - p2 - p3;
+    //cerr << "add: delta = " << delta << endl;
+    return delta * C[contest] + S[day][contest];
+}
+
 vector<Contest> simulated_annealing(const Stopwatch& stopwatch, int64_t time_limit_millis) {
     auto schedule = make_initial_solution();
+    vector<set<int>> contest_date_list(26);
     auto current_score = calculate_score(schedule);
     vector<Contest> best_schedule;
     int64_t best_score = numeric_limits<int64_t>::min();
@@ -63,6 +116,16 @@ vector<Contest> simulated_annealing(const Stopwatch& stopwatch, int64_t time_lim
     const double final_temperature = 100.0;
     double temperature = initial_temperature;
     double start_time = stopwatch.elapsed_millis();
+
+    // contest_date_list を初期化
+    for(Contest c = 0; c < 26; ++c) {
+        contest_date_list[c].insert(-1);
+        contest_date_list[c].insert(n_days);
+    }
+    for(int d = 0; d < n_days; ++d) {
+        Contest contest = schedule[d];
+        contest_date_list[contest].insert(d);
+    }
 
     for(int iteration = 0; ; ++iteration) {
         if(iteration % 100 == 0) {
@@ -82,19 +145,36 @@ vector<Contest> simulated_annealing(const Stopwatch& stopwatch, int64_t time_lim
         enum { kSwap, kReplace } action;
         Contest old;
         int day;
+        int64_t new_score;
         if(rng.next_double() < 0.5) {
             // 選んだ日のコンテストをランダムに置き換える
             action = kReplace;
             day = rng() % n_days;
             old = schedule[day];
             schedule[day] = rng() % 26;
+            new_score = current_score +
+                calculate_score_delta_on_contest_remove(contest_date_list, day, old) +
+                calculate_score_delta_on_contest_add(contest_date_list, day, schedule[day]);
         } else {
             // 選んだ日のコンテストを次の日と入れ替える
             action = kSwap;
             day = rng() % (n_days - 1);
+            new_score = current_score +
+                calculate_score_delta_on_contest_remove(contest_date_list, day, schedule[day]) +
+                calculate_score_delta_on_contest_remove(contest_date_list, day+1, schedule[day+1]) +
+                calculate_score_delta_on_contest_add(contest_date_list, day, schedule[day+1]) +
+                calculate_score_delta_on_contest_add(contest_date_list, day+1, schedule[day]);
             swap(schedule[day], schedule[day+1]);
         }
-        auto new_score = calculate_score(schedule);
+        /*
+        cerr << "schedule = " << schedule << endl;
+        cerr << "i = " << iteration << ", " << action << endl;
+        if(new_score != calculate_score(schedule)) {
+            cerr << "i = " << iteration << ", action = " << action << ", day = " << day << ", old = " << old << ", new = " << schedule[day] << endl;
+            cerr << "new_score = " << new_score << ", score = " << calculate_score(schedule) << ", current = " << current_score << endl;
+            throw runtime_error("abort");
+        }
+        */
 
         // 遷移するか決める
         bool ok;
@@ -115,11 +195,25 @@ vector<Contest> simulated_annealing(const Stopwatch& stopwatch, int64_t time_lim
         } else { // 遷移しない
             // 状態を元に戻す
             if(action == kReplace) {
+                Contest curr = schedule[day];
+                contest_date_list[curr].erase(day);
+                contest_date_list[old].insert(day);
                 schedule[day] = old;
             } else {
+                contest_date_list[schedule[day]].erase(day);
+                contest_date_list[schedule[day+1]].erase(day+1);
+                contest_date_list[schedule[day]].insert(day+1);
+                contest_date_list[schedule[day+1]].insert(day);
                 swap(schedule[day], schedule[day+1]);
             }
         }
+
+        /*
+        if(current_score != calculate_score(schedule)) {
+            cerr << "i = " << iteration << ", action = " << action << ", day = " << day << ", old = " << old << endl;
+            throw runtime_error("abort2");
+        }
+        */
     }
 }
 
--- intro-heuristics/C.cpp
+++ intro-heuristics/C.cpp
@@ -1,4 +1,5 @@
 #include "lib/prelude.hpp"
+#include "lib/debug.hpp"
 
 using Day = int;
 using Contest = int;
@@ -37,6 +38,54 @@ int64_t calculate_score(const vector<int>& schedule) {
     return satisfaction;
 }
 
+inline int64_t calculate_penalty(int k) {
+    return k * (k+1) / 2;
+}
+
+int64_t calculate_score_delta_on_contest_remove(
+    vector<set<int>>& contest_date_list, int day, Contest contest
+) {
+    auto& l = contest_date_list[contest];
+    auto it = l.find(day);
+    int prev_day = *prev(it);
+    int next_day = *next(it);
+
+    int d1 = next_day - prev_day - 1; // コンテストが開催されなかった日数
+    int d2 = day - prev_day - 1;
+    int d3 = next_day - day - 1;
+
+    int64_t p1 = calculate_penalty(d1);
+    int64_t p2 = calculate_penalty(d2);
+    int64_t p3 = calculate_penalty(d3);
+
+    l.erase(day);
+
+    int64_t delta = p2 + p3 - p1;
+    return delta * C[contest] - S[day][contest];
+}
+
+int64_t calculate_score_delta_on_contest_add(
+    vector<set<int>>& contest_date_list, int day, Contest contest
+) {
+    auto& l = contest_date_list[contest];
+    auto it = l.lower_bound(day);
+    int next_day = *it;
+    int prev_day = *prev(it);
+
+    int d1 = next_day - prev_day - 1; // コンテストが開催されなかった日数
+    int d2 = day - prev_day - 1;
+    int d3 = next_day - day - 1;
+
+    int64_t p1 = calculate_penalty(d1);
+    int64_t p2 = calculate_penalty(d2);
+    int64_t p3 = calculate_penalty(d3);
+
+    l.insert(day);
+
+    int64_t delta = p1 - p2 - p3;
+    return delta * C[contest] + S[day][contest];
+}
+
 int main() {
     read_input();
 
@@ -44,12 +93,29 @@ int main() {
     vector<Contest> schedule(n_days);
     REP(d, n_days) schedule[d] = read_int()-1;
 
+    // contest_date_list を初期化
+    vector<set<int>> contest_date_list(26);
+    for(Contest c = 0; c < 26; ++c) {
+        contest_date_list[c].insert(-1);
+        contest_date_list[c].insert(n_days);
+    }
+    for(int d = 0; d < n_days; ++d) {
+        Contest contest = schedule[d];
+        contest_date_list[contest].insert(d);
+    }
+
     // process query
     int M = read_int();
+    int64_t current_score = calculate_score(schedule);
+    //cerr << "init: " << current_score << '\n';
     REP(m, M) {
-        int d = read_int()-1;
-        int q = read_int()-1;
-        schedule[d] = q;
-        cout << calculate_score(schedule) << '\n';
+        int day = read_int()-1;
+        int old = schedule[day];
+        schedule[day] = read_int()-1;
+        int64_t new_score = current_score +
+                calculate_score_delta_on_contest_remove(contest_date_list, day, old) +
+                calculate_score_delta_on_contest_add(contest_date_list, day, schedule[day]);
+        cout << new_score << '\n';
+        current_score = new_score;
     }
 }
