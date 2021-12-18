019f573860aa97a0f2a9ed6bab704c1f25129e63
Yusuke Nojima <nojima@ynojima.com>
2021-12-19 00:24:51 +0900

    move EDPC to DP

--- DP/K.cpp
+++ DP/K.cpp
@@ -1,11 +1,28 @@
 #include "lib/prelude.hpp"
 
 int main() {
-    auto N = read_int();
-    auto K = read_int();
-    vector<int64_t> A(N);
+    int N = read_int();
+    int K = read_int();
+    vector<int> A(N);
     REP(i, N) A[i] = read_int();
 
-    // dp[i][k]: 
-    vector dp(N, vector(K+1, bool()));
+    // dp[k]: 山にk個石があるとき、手番のプレイヤーの勝ちか？
+    vector<bool> dp(K+1);
+
+    REP(k, K+1) {
+        // 後退解析: 遷移先の状態がすべて勝ちである場合、負け。そうでないなら勝ち。
+        bool all_win = true;
+        bool can_move = false;
+        for (int a : A) if (k-a >= 0) {
+            can_move = true;
+            if (!dp[k-a]) { all_win = false; }
+        }
+        dp[k] = can_move && !all_win;
+    }
+
+    if (dp[K]) {
+        cout << "First" << LF;
+    } else {
+        cout << "Second" << LF;
+    }
 }
